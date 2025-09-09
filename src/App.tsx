d723883c82981db79edd7686d085d7b9fdd13d86
kateamartell <kate.martell@maine.edu>
2025-09-08 21:31:17 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): React.JSX.Element {
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
+                hello world
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
