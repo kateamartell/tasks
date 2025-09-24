1b3d63c2bae33f85c3c0ec9d79416042949fd598
kateamartell <kate.martell@maine.edu>
2025-09-24 19:54:25 -0400

    passed makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -82,7 +82,12 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0){
+        return "0=0";
+    }
+    const sum = addends.reduce ((total:number, num: number)=> total + num, 0);
+    const numString = addends.join("+");
+    return sum + "=" + numString;
 }
 
 /**
