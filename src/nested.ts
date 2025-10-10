4c42b9d9779276eb30e7f8a87c907b70c7570fa6
kateamartell <kate.martell@maine.edu>
2025-10-10 14:02:47 -0400

    Passed getNames

--- src/nested.ts
+++ src/nested.ts
@@ -46,7 +46,7 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * Do not modify the input array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    return questions.map((q: Question): string => q.name);
 }
 
 /**
