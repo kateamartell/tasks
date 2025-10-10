22243ed0abd42cfea08eeabd3c92bc1115f11920
kateamartell <kate.martell@maine.edu>
2025-10-10 13:57:23 -0400

    Passed  getNonEmptyQuestions

--- src/nested.ts
+++ src/nested.ts
@@ -15,7 +15,8 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    return questions.filter((q: Question): boolean => q.body !== "" || q.expected !== "" || 
+    q.options.length >0);
 }
 
 /***
