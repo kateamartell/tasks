17fe915644adb7ebb83e4735b3dc088c823655d8
kateamartell <kate.martell@maine.edu>
2025-10-18 15:50:42 -0400

    Passed TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-/**
- * Here is a helper function you *must* use to "roll" your die.
- * The function uses the builtin `random` function of the `Math`
- * module (which returns a random decimal between 0 up until 1) in order
- * to produce a random integer between 1 and 6 (inclusive).
- */
+
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(3);
+    const [rightDie, setRightDie] = useState<number>(2);
+
+    const win = leftDie === rightDie;
+    const lose = leftDie === 1 && rightDie === 1;
+
+    return (
+        <div>
+            <div>
+                Die 1 : <span data-testid="left-die">{leftDie}</span> Die 2:{" "}
+                <span data-testid="right-die">{rightDie}</span>
+            </div>
+            <div>
+                {lose ? "You Lose" : win ? "You win" : ""}
+            </div>
+            <Button onClick={() => setLeftDie(d6())}>Roll Left</Button>
+            <Button onClick={() => setRightDie(d6())}>Roll Right</Button>
+        </div>
+    );
 }
