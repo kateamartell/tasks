ff9878fcf5e17c927ace5f5478f0736294a88706
kateamartell <kate.martell@maine.edu>
2025-10-19 18:43:26 -0400

    fixed counter, asccidently edited before, put back to original

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,10 +4,9 @@ import { Button } from "react-bootstrap";
 export function Counter(): React.JSX.Element {
     const [value, setValue] = useState<number>(0);
     return (
-        <div>
-            <div>Count: {value}</div>
-            <Button onClick={() => setValue(value + 1)}>Add One</Button>
-            <Button onClick={() => setValue(value - 1)}>Subtract One</Button>
-        </div>
+        <span>
+            <Button onClick={() => {setValue(value + 1)}}>Add One</Button>
+            to {value}
+        </span>
     );
 }
